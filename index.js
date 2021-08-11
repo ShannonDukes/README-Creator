263ff35b3bf6b0ba503f5a68be2073d460f67bb8
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-08-10 18:13:14 -0700

    commenting out functions for now

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ questions()
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
